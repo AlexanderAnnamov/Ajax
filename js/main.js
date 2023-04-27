51dfaf3a87b02502237e57a330d6ea659b62eda5
Alexander <hash_nam@mail.ru>
2023-04-27 15:59:09 +0500

    custom get request

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
   function req() {
-    fetch("http://localhost:3000/people")
-      .then((data) => data.json())
+    getResource("http://localhost:3000/people")
       .then((data) => createCards(data))
       .catch((err) => console.error(err));
 
@@ -11,6 +10,16 @@ window.addEventListener("DOMContentLoaded", () => {
   document
     .querySelector("button")
     .addEventListener("click", req, { once: true });
+
+  async function getResource(url) {
+    const res = await fetch(`${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
 
   function createCards(response) {
     response.forEach((item) => {
