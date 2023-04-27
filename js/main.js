2d026cd319072241ed3282d9babcc61f94c9d110
Alexander <hash_nam@mail.ru>
2023-04-27 14:49:25 +0500

    optimization method

--- js/main.js
+++ js/main.js
@@ -8,27 +8,7 @@ window.addEventListener("DOMContentLoaded", () => {
       if (request.status == 200) {
         let data = JSON.parse(request.response);
         console.log(data);
-
-        data.forEach((item) => {
-          let card = document.createElement("div");
-
-          card.classList.add("card");
-
-          let icon;
-          if (item.sex === "male") {
-            icon = "img/icons/mars.png";
-          } else {
-            icon = "img/icons/female.png";
-          }
-
-          card.innerHTML = `<img src="${item.photo}" alt="photo">
-            <div class="name">${item.name} ${item.surname}</div>
-            <div class="sex">
-                <img src=${icon} alt="male">
-            </div>
-            <div class="age">${item.age}</div>`;
-          document.querySelector(".app").appendChild(card);
-        });
+        createCards(data);
       } else {
         console.error("Что-то пошло не так!");
       }
@@ -39,4 +19,27 @@ window.addEventListener("DOMContentLoaded", () => {
   document
     .querySelector("button")
     .addEventListener("click", req, { once: true });
+
+  function createCards(response) {
+    response.forEach((item) => {
+      let card = document.createElement("div");
+
+      card.classList.add("card");
+
+      let icon;
+      if (item.sex === "male") {
+        icon = "img/icons/mars.png";
+      } else {
+        icon = "img/icons/female.png";
+      }
+
+      card.innerHTML = `<img src="${item.photo}" alt="photo">
+          <div class="name">${item.name} ${item.surname}</div>
+          <div class="sex">
+              <img src=${icon} alt="male">
+          </div>
+          <div class="age">${item.age}</div>`;
+      document.querySelector(".app").appendChild(card);
+    });
+  }
 });
